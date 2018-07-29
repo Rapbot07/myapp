74ac8d43d9ff8966036fe97f68a8b5ee4d28197a
Rapbot07 <enochlian15@gmail.com>
2018-07-29 16:22:59 +0800

    Hello,Master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, Enoch!jniun"
+  	render html: "hello, I am Enoch!"
   end
 end
