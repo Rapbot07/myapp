846b89746da3b9ecd49ff48d41b305f82af772a3
Rapbot07 <enochlian15@gmail.com>
2018-09-01 11:36:03 +0800

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,15 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
- def index
-   @users = User.paginate(page: params[:page])
- end
+end
 
- def show
-  	@user = User.find(params[:id])
+  def index
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  end
+
+  def show
+  	 @user = User.find(params[:id])
+     redirect_to root_url and return unless FILL_IN     
   end
 
   def new
@@ -17,9 +20,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)		#Not the final implementation!
   	if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
    	else
   	  render 'new'
   	end
@@ -44,10 +47,10 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
   def logged_in_user
     unless logged_in?
